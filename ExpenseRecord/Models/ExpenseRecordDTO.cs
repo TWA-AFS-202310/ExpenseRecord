5c3e3f71963d64ffaf1731969a8a5396d2d38123
Diana Jing <DJing@slb.com>
2023-10-26 13:19:10 +0800

    fix DTO type

--- ExpenseRecord/Models/ExpenseRecordDTO.cs
+++ ExpenseRecord/Models/ExpenseRecordDTO.cs
@@ -4,15 +4,15 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace ExpenseRecord.Models
 {
-    public class ExpenseRecordDB
+    public class ExpenseRecordDTO
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         [Required]
         [StringLength(50)]
         public string Description { get; set; } = string.Empty;
-        public double Amount { get; set; }
+        public int Amount { get; set; }
         public string Type { get; set; } = string.Empty;
-        [BsonRepresentation(BsonType.String)]
-        public DateTime Time { get; set; } = DateTime.UtcNow;
+        ]
+        public string Time { get; set; } = string.Empty;
     }
 }
