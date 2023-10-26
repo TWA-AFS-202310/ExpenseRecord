d56cee605fe113b12c901dc7c23c1caa0cf3d62f
Diana Jing <DJing@slb.com>
2023-10-26 12:50:29 +0800

    Add expense record model

---
+++ ExpenseRecord/Models/ExpenseRecordDB.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ExpenseRecord.Models
+{
+    public class ExpenseRecordDB
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; } = string.Empty;
+        public double Amount { get; set; }
+        public string Type { get; set; } = string.Empty;
+        [BsonRepresentation(BsonType.String)]
+        public DateTime Time { get; set; } = DateTime.UtcNow;
+    }
+}
