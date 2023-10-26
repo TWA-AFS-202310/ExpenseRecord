f7b52dd653ab87ee61d98bd7c965955a80c41df0
Diana Jing <DJing@slb.com>
2023-10-26 13:41:04 +0800

    add expense record service

---
+++ ExpenseRecord/services/ExpenseRecordService.cs
@@ -0,0 +1,39 @@
+﻿
+using ExpenseRecord.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ExpenseRecord.services
+
+
+{
+    public class ExpenseRecordService:IExpenseRecordService
+    {
+        private static readonly List<ExpenseRecordDTO> _expenseRecords = new();
+
+        public Task CreateAsync(ExpenseRecordDTO newRecord)
+        {
+            _expenseRecords.Add(newRecord);
+            return Task.CompletedTask;
+        }
+
+
+        public Task<bool> RemoveAsync(string id)
+        {
+            var itemToBeRemoved = _expenseRecords.Find(x => x.Id == id);
+            if (itemToBeRemoved is null)
+            {
+                return Task.FromResult(false);
+            }
+            _expenseRecords.Remove(itemToBeRemoved);
+            return Task.FromResult(true);
+        }
+
+        public Task<List<ExpenseRecordDTO>> GetAsync()
+        {
+            return Task.FromResult(_expenseRecords);
+
+        }
+
+    }
+}
---
+++ ExpenseRecord/services/IExpenseRecordService.cs
@@ -0,0 +1,11 @@
+﻿using ExpenseRecord.Models;
+
+namespace ExpenseRecord.services
+{
+    public interface IExpenseRecordService 
+    {
+        Task CreateAsync(ExpenseRecordDTO newRecord);
+        Task<List<ExpenseRecordDTO>> GetAsync();
+        Task<bool> RemoveAsync(string id);
+    }
+}
