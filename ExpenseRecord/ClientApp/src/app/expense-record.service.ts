95c338aeac24580b311062ad508e52770073c3b2
JWei9 <JWei9@slb.com>
2023-10-26 17:45:19 +0800

    last bug

---
+++ ExpenseRecord/ClientApp/src/app/expense-record.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ExpenseRecordService {
+  private apiUrl = 'api/ExpenseRecord';
+
+  constructor(private http: HttpClient) { }
+
+  getExpenseRecords(): Observable<ExpenseRecord[]> {
+    return this.http.get<ExpenseRecord[]>(this.apiUrl);
+  }
+
+  createExpenseRecord(expenseRecord: ExpenseRecord): Observable<ExpenseRecord> {
+    return this.http.post<ExpenseRecord>(this.apiUrl, expenseRecord);
+  }
+
+  deleteExpenseRecord(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<any>(url);
+  }
+}
+
+export interface ExpenseRecord {
+  Description: string;
+  Type: string;
+  Amount: number;
+  Date: string;
+}
---
+++ ExpenseRecord/Controllers/ExpenseRecordController.cs
@@ -0,0 +1,39 @@
+﻿namespace ExpenseRecord.Controllers
+{
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpenseRecordController : ControllerBase
+{
+    private static List<ExpenseRecord> expenseRecords = new List<ExpenseRecord>();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<ExpenseRecord>> GetExpenseRecords()
+    {
+        return Ok(expenseRecords);
+    }
+
+    [HttpPost]
+    public IActionResult AddExpenseRecord(ExpenseRecord expenseRecord)
+    {
+        expenseRecords.Insert(0, expenseRecord);
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult DeleteExpenseRecord(int index)
+    {
+        if (index >= 0 && index < expenseRecords.Count)
+        {
+            expenseRecords.RemoveAt(index);
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+}
+}
