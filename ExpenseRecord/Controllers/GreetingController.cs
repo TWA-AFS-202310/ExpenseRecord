1e745e3dc276d5c5702e7db3d51c9c001cf0ae3d
Diana Jing <DJing@slb.com>
2023-10-26 13:59:33 +0800

    add http methods to controller

--- ExpenseRecord/Controllers/GreetingController.cs
+++ ExpenseRecord/Controllers/GreetingController.cs
@@ -1,3 +1,5 @@
+using ExpenseRecord.Models;
+using ExpenseRecord.services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseRecord.Controllers;
@@ -6,10 +8,54 @@ namespace ExpenseRecord.Controllers;
 [Route("[controller]")]
 public class GreetingController : ControllerBase
 {
-    [HttpGet]
-    public string greet(string name)
+    private readonly IExpenseRecordService _expenseRecordService;
+    public GreetingController(IExpenseRecordService expenseRecordService)
     {
-        Console.Out.WriteLine(name);
-        return "Hello, " + name;
+        _expenseRecordService = expenseRecordService;
     }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<ExpenseRecordDTO>), 200)]
+    [ProducesResponseType(500)]
+
+    public ActionResult<ExpenseRecordDTO> GetAll()
+    {
+
+        var records = _expenseRecordService.GetAll();
+
+        return Ok(records);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ExpenseRecordDTO), 201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(500)]
+    public ActionResult<ExpenseRecordDTO> Post([FromBody] ExpenseRecordDTO expenseRecordInput)
+    {
+        var toDoItemDto = new ExpenseRecordDTO
+        {   
+            Description = expenseRecordInput.Description,
+            Type = expenseRecordInput.Type,
+            Amount = expenseRecordInput.Amount,
+            Time = expenseRecordInput.Time
+        };
+        _expenseRecordService.CreateRecord(expenseRecordInput);
+        return Created("", expenseRecordInput);
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(500)]
+    public ActionResult DeleteAsync(string id)
+    {
+        var isSuccessful = _expenseRecordService.RemoveRecord(id);
+        if (!isSuccessful)
+        {
+            return NotFound($"The item with id {id} does not exist.");
+        }
+        return NoContent();
+    }
+
+
 }
